7a50bfab20efa5fe5c746daeea3f8b05fee46a02
Yuriy Pavlenko <yurii.pavlenko@mustpay.tech>
2022-06-26 16:15:27 +0300

    ChocolateBoiler - Singleton

---
+++ src/ChocolateFactory/ChocolateBoiler.java
@@ -0,0 +1,53 @@
+package ChocolateFactory;
+
+public class ChocolateBoiler {
+    private static ChocolateBoiler boiler;
+    private boolean empty;
+    private boolean boiled;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (boiler == null) {
+            synchronized (ChocolateBoiler.class) {
+                if (boiler == null) {
+                    boiler = new ChocolateBoiler();
+                }
+            }
+        }
+        return boiler;
+    }
+
+    public void fill() {
+        if(isEmpty()) {
+            empty = false;
+            boiled = false;
+            System.out.println("Filling boiler by the chocolate and milk");
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            System.out.println("Draining hot milk and chocolate");
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            System.out.println("Boiling milk and chocolate");
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
